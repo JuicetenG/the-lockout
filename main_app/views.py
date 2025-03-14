34b6b168272692e45ffbb953dd1f4a170934e090
JuicetenG <127890628+JuicetenG@users.noreply.github.com>
2025-03-14 14:51:24 -0400

    add class based view for create and test functionality

--- main_app/views.py
+++ main_app/views.py
@@ -3,18 +3,25 @@ from django.views.generic.edit import CreateView
 from django.contrib.auth.views import LoginView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 from .models import Gear
 
 class Home(LoginView):
     template_name = 'home.html'
 
 def gear_index(request):
-    gear_items = Gear.objects.all()
+    gear_items = Gear.objects.filter(user=request.user)
     return render(request, 'gear/gear-index.html', {'gear': gear_items})
 
 class GearCreate(CreateView):
     model = Gear
-    fields = '__all__'
+    fields = ['make', 'model', 'type', 'cost', 'trade', 'image']
+    success_url = '/gear/' 
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user 
+        return super().form_valid(form)
+    
 
 def signup(request):
     error_message = ''
@@ -23,7 +30,7 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('cat-index')
+            return redirect('gear-index')
         else:
             error_message = 'Invalid sign up - try again'
 
