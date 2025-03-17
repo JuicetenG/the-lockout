83bc73fa0d13cc3a305b8766271d774e1d8b254a
JuicetenG <127890628+JuicetenG@users.noreply.github.com>
2025-03-17 15:55:38 -0400

    style all pages except for confirm delete

--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,8 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.views import LoginView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from .models import Gear
 from .mixins import ImageUploadMixin
@@ -11,24 +13,26 @@ from .mixins import ImageUploadMixin
 class Home(LoginView):
     template_name = 'home.html'
 
+@login_required
 def gear_index(request):
     gear_items = Gear.objects.filter(user=request.user)
     return render(request, 'gear/gear-index.html', {'gear': gear_items})
 
-class GearCreate(ImageUploadMixin, CreateView):
+class GearCreate(ImageUploadMixin, LoginRequiredMixin, CreateView):
     model = Gear
     fields = ['make', 'model', 'type', 'cost', 'trade']
     success_url = '/gear/' 
 
-class GearUpdate(ImageUploadMixin, UpdateView):
+class GearUpdate(ImageUploadMixin, LoginRequiredMixin, UpdateView):
     model = Gear
     fields = [ 'cost', 'trade']
     success_url = '/gear/'
 
-class GearDelete(DeleteView):
+class GearDelete(LoginRequiredMixin, DeleteView):
     model = Gear
     success_url = '/gear/'
-    
+
+@login_required
 def gear_detail(request, gear_id):
     gear = Gear.objects.get(id=gear_id)
     return render(request, 'gear/gear-detail.html', {'gear': gear})
